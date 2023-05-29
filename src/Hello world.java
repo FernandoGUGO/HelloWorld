e6c4e71a4aa0c7361a1d5e6cf1d5a8b1dda09465
Ricardo <ritchie.owen07@hotmail.com>
2023-05-29 16:47:44 -0600

    change lenguage

---
+++ src/Hello world.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hello World!");
+
+        //Put your name on project and write some messages that you want, remember make a pull request to upload your changes.
+
+
+        for (int i = 1; i <= 5; i++) {
+
+            System.out.println("i = " + i);
+        }
+    }
+}
