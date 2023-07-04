9837b2414757b7694c888faaa587b302ca142733
Carlos <138539548+CarlosOB16@users.noreply.github.com>
2023-07-04 14:13:36 -0600

    my name and first commit

--- src/Hello world.java
+++ src/Hello world.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello World!");
+        System.out.printf("Hello World! I am Carlos");
 
         //Put your name on project and write some messages that you want, remember make a pull request to upload your changes.
 
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 4; i++) {
 
             System.out.println("i = " + i);
         }
